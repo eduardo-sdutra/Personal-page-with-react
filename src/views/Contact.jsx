d3dee96020051715797dadb8da451848194047a0
Eduardo Soares Dutra <eduardosoaresdutra@gmail.com>
2022-03-22 21:48:04 -0300

    Additions on contact section

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,11 +1,22 @@
 import { CardTemplate } from "../components/CardTemplate"
 import { SectionContainer } from "../components/SectionContainer"
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+
+`
 
 const Contact = props => {
     return (
         <SectionContainer>
             <CardTemplate>
-                
+                <Wrapper>
+                    <div className="infoWrapper">
+                        <h1>Meus Contatos</h1>
+                        <hr/>
+                        
+                    </div>
+                </Wrapper>
             </CardTemplate>
         </SectionContainer>
     )
